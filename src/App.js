3b33522c85820be669ac905446f6f6f92ea43370
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2024-10-16 21:24:06 -0400

    Nearest Click Calculate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react'
 import './App.css';
 
-function App() {
+export default function App() {
+  let text = useRef(null);
+  const handleClick = (event) => {
+
+    let blks = document.getElementsByClassName('block');
+
+    const blocks = [...event.target.children]; // [...blks]
+
+    const { clientX, clientY } = event;
+
+    const dist = {};
+
+    for (const block of blocks) {
+      const { x, y } = block.getBoundingClientRect();
+      console.log(x, y, block);
+
+      const distance = Math.sqrt(
+        Math.pow(x - clientX, 2) + Math.pow(y - clientY, 2)
+      );
+
+      dist[block.innerHTML] = distance;
+    }
+
+    console.log(clientX, clientY, dist);
+
+    let max = Number.MAX_VALUE,
+      tex = '';
+
+    for (const [key, val] of Object.entries(dist)) {
+      if (val < max) {
+        max = val;
+        tex = key;
+      }
+    }
+
+    console.log(max, tex, 'Result');
+
+    text.current.innerHTML = tex;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div onClick={handleClick} className="clickArea">
+        <div className="block">A</div>
+        <div className="block">B</div>
+        <div className="block">C</div>
+        <div className="block">D</div>
+        <div className="block">E</div>
+      </div>
+      <p ref={text}>A</p>
+    </>
   );
 }
-
-export default App;
